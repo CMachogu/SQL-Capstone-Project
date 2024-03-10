0a7fee3891d432bf4837d8618fbe6f71f75018ab
Cyrus Machogu Nyakang'i <159150903+CMachogu@users.noreply.github.com>
2024-03-10 15:21:37 +0300

    Add files via upload

---
+++ Capstone.sql
@@ -0,0 +1,37 @@
+
+--CREATE TABLE Athens(
+--real_sum FLOAT,
+--room_type VARCHAR (50),
+--room_shared VARCHAR (15),
+--person_capacity INT,
+--guest_satisfaction_overall INT,
+--bedrooms INT
+--);
+
+--INSERT INTO Paris (real_sum, room_type, person_capacity, guest_satisfaction_overall, bedrooms) 
+--VALUES
+--(296.1599403, 'Privateroom', 2, 97, 1), 
+--(288.2374872, 'Privateroom', 2, 97, 1), 
+--(211.3430888, 'Privateroom', 2, 94, 1), 
+--(298.9561003, 'Entirehomeapt', 2, 91, 1), 
+--(247.9261814, 'Entirehomeapt', 4, 82, 1), 
+--(527.0761488, 'Entirehomeapt', 4, 93, 1), 
+--(193.6340759, 'Privateroom', 2, 90, 1), 
+--(207.1488489, 'Entirehomeapt', 2, 95, 1), 
+--(345.0927393, 'Entirehomeapt', 2, 94, 0), 
+--(741.4484108, 'Entirehomeapt', 3, 91, 1), 
+--(350.9180725, 'Privateroom', 2, 99, 1), 
+--(239.3046882, 'Entirehomeapt', 2, 89, 0), 
+--(242.7998882, 'Privateroom', 2, 99, 1), 
+--(242.7998882, 'Privateroom', 2, 97, 1), 
+--(845.6053686, 'Entirehomeapt', 4, 80, 1), 
+--(277.9849007, 'Privateroom', 2, 96, 1), 
+--(301.0532202, 'Entirehomeapt', 2, 97, 0), 
+--(301.0532202, 'Entirehomeapt', 2, 97, 1), 
+--(382.1418585, 'Privateroom', 2, 90, 1);
+
+----DROP TABLE Rome;
+----DROP TABLE Vienna;
+----DROP TABLE Amsterdam;
+----DROP TABLE Athens;
+
